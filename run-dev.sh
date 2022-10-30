44bb4e68cb9de6c6d7357713c664494870d7435f
bigfoodK <38313680+bigfoodK@users.noreply.github.com>
2022-10-31 05:40:47 +0900

    Run docker with current user id (#10)

--- run-dev.sh
+++ run-dev.sh
@@ -1,1 +1,6 @@
-docker-compose -f docker-compose.dev.yml up
+USER_ID=$(id -u):$(id -g)\
+&& mkdir -p server/node_modules\
+&& mkdir -p client/node_modules\
+&& mkdir -p client/src/api\
+&& mkdir -p data\
+&& docker-compose -f docker-compose.dev.yml up
