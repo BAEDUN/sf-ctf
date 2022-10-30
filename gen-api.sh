4d844303c581ace9df79bc1a97e7f7f58ecc9aac
bigfoodK <satcom1600@hanmail.net>
2022-10-30 20:05:35 +0000

    Run docker with current user id

--- gen-api.sh
+++ gen-api.sh
@@ -1,1 +1,6 @@
-docker-compose -f docker-compose.gen-api.yml up --abort-on-container-exit --exit-code-from generator
+USER_ID=$(id -u):$(id -g)\
+&& mkdir -p server/node_modules\
+&& mkdir -p client/node_modules\
+&& mkdir -p client/src/api\
+&& mkdir -p data\
+&& docker-compose -f docker-compose.gen-api.yml up --abort-on-container-exit --exit-code-from generator
