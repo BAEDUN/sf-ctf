8ecc13ed70f863ec3b4ccf28f8d69222c0067f50
hubris <darmaa333@gmail.com>
2022-11-03 10:50:18 +0900

    Remove admin things

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,6 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
   app.setGlobalPrefix("api");
   serveSwagger(app);
   await app.listen(3000);
