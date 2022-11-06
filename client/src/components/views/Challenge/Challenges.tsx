0be6573517245c7f3ac28381600e7a4e10545446
hubris <darmaa333@gmail.com>
2022-11-06 21:50:59 +0900

    WIP

--- client/src/components/views/Challenge/Challenges.tsx
+++ client/src/components/views/Challenge/Challenges.tsx
@@ -1,30 +1,31 @@
 import React, { useState, useMemo, useEffect, useCallback, useContext } from 'react';
-import { MDBInput } from 'mdb-react-ui-kit';
 import { ToastContainer, toast } from 'react-toastify';
 import "./Challenges.css";
-import { ChallengeApi, GetAllChallengesResponseDtoChallengesInner } from "../../../api"
+import { ChallengeApi, GetAllChallengesResponseDtoChallengesInner, CreateChallengeRequestDtoCategoryEnum } from "../../../api"
 import { AuthContext } from '../../../context/AuthProvider';
-import { useNavigate } from 'react-router-dom';
 import Challenge from './Challenge';
 
-const loadStates = {
-    pending: 0,
-    notStarted: 1,
-    loaded: 2
+type Categories = {
+    [CreateChallengeRequestDtoCategoryEnum.Forensic]: boolean;
+    [CreateChallengeRequestDtoCategoryEnum.Misc]: boolean;
+    [CreateChallengeRequestDtoCategoryEnum.Pwnable]: boolean;
+    [CreateChallengeRequestDtoCategoryEnum.Reversing]: boolean;
+    [CreateChallengeRequestDtoCategoryEnum.Web]: boolean;
+}
+const defaultCategories: Categories = {
+    [CreateChallengeRequestDtoCategoryEnum.Forensic]: false,
+    [CreateChallengeRequestDtoCategoryEnum.Misc]: false,
+    [CreateChallengeRequestDtoCategoryEnum.Pwnable]: false,
+    [CreateChallengeRequestDtoCategoryEnum.Reversing]: false,
+    [CreateChallengeRequestDtoCategoryEnum.Web]: false,
 }
 
 export default function Challenges() {
-    const navigate = useNavigate();
-    const challengePageState = useMemo<{
-        categories: { [key: string]: boolean }
-        showSolved: boolean,
-    }>(() => JSON.parse(localStorage.getItem('challengePageState') || '{}'), []);
     const [challenges, setChallenges] = useState<GetAllChallengesResponseDtoChallengesInner[]>([]);
-    const [categories, setCategories] = useState(challengePageState.categories || {});
-    const [showSolved, setShowSolved] = useState(challengePageState.showSolved || false);
+    const [categories, setCategories] = useState<Categories>(defaultCategories);
+    const [showSolved, setShowSolved] = useState(false);
     const [solvedChallengeTitles, setSolvedChallengeTitles] = useState<string[]>([]);
-    const [loadState, setLoadState] = useState(loadStates.pending);
-    const { auth, setAuth } = useContext(AuthContext);
+    const { auth } = useContext(AuthContext);
 
     const setSolved = useCallback((title: string) => {
         setSolvedChallengeTitles(solvedChallengeTitles => {
@@ -35,24 +36,16 @@ export default function Challenges() {
         })
     }, []);
 
-    const handleShowSolvedChange = useCallback((e: any) => {
+    const handleShowSolvedChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
         setShowSolved(e.target.checked)
     }, []);
-    const handleCategoryCheckedChange = useCallback((e: any) => {
-        setCategories((categories: any) => ({
-            ...categories,
-            [e.target.dataset.category]: e.target.checked
-        }));
-    }, []);
-
-    if (!auth) {
-        navigate("/", { replace: true });
-        return null;
-    }
 
     useEffect(() => {
+        if (!auth) {
+            return;
+        }
         const action = async () => {
-            const challengeApi = new ChallengeApi()
+            const challengeApi = new ChallengeApi(undefined, location.origin)
             const response = await challengeApi.challengeControllerGetAll({
                 accessToken: auth.token
             });
@@ -64,7 +57,7 @@ export default function Challenges() {
             setChallenges(challenges);
         }
         action();
-    }, []);
+    }, [auth]);
 
     // useEffect(() => {
     //     const action = async () => {
@@ -79,45 +72,31 @@ export default function Challenges() {
     //     action()
     // }, [toast])
 
-    useEffect(() => {
-        localStorage.challPageState = JSON.stringify({ categories, showSolved });
-    }, [categories, showSolved]);
-
     const challengesToDisplay = useMemo(() => {
         let filtered = challenges
         if (!showSolved) {
             filtered = filtered.filter(challenge => !solvedChallengeTitles.includes(challenge.title!))
         }
-        let filterCategories = false
-        Object.values(categories).forEach(displayCategory => {
-            if (displayCategory) filterCategories = true
-        })
-        if (filterCategories) {
-            Object.keys(categories).forEach(category => {
-                if (categories[category] === false) {
-                    // Do not display this category
-                    filtered = filtered.filter(challenge => challenge.category !== category)
-                }
-            })
+        const shouldFilter = Object.values(categories).some(checked => checked);
+        if (shouldFilter) {
+            filtered = filtered.filter(challenge => categories[challenge.category!])
         }
-
         filtered.sort((a, b) => {
             return a.score! - b.score!;
         })
-
         return filtered;
     }, [challenges, categories, showSolved, solvedChallengeTitles])
 
     const { categoryCounts, solvedCount } = useMemo(() => {
-        const categoryCounts = new Map()
-        let solvedCount = 0
+        const categoryCounts = new Map();
+        let solvedCount = 0;
         if (challenges !== null) {
             for (const challenge of challenges) {
                 if (!categoryCounts.has(challenge.category)) {
                     categoryCounts.set(challenge.category, {
                         total: 0,
                         solved: 0
-                    })
+                    });
                 }
 
                 const solved = solvedChallengeTitles.includes(challenge.title!)
@@ -134,49 +113,56 @@ export default function Challenges() {
         return { categoryCounts, solvedCount }
     }, [challenges, solvedChallengeTitles])
 
+    const CategoryCheck = useCallback((props: { category: CreateChallengeRequestDtoCategoryEnum }) => {
+        const { category } = props;
+        return <div className='categoryCheck'>
+            <input id={`category-${category}`} className='formInput' type='checkbox' checked={categories[category]} onChange={(event) => setCategories(categories => {
+                return { ...categories, [category]: event.target.checked };
+            })} />
+            <label htmlFor={`category-${category}`} className='formLabel'>{category} ({categoryCounts.get(category)?.solved || 0}/{categoryCounts.get(category)?.total || 0} solved)</label>
+        </div>
+    }, [categories, categoryCounts]);
+
     return (
         <div className='Challenges'>
-            <div className="ChallWrap">
-                <div className="ChallFrame">
-                    <div className="frameBody">
-                        <div className="frameTitle">Filter</div>
-                        <div className="showSolved">
-                            <div className="formCheck">
-                                <input type="checkbox" id="show_solved" className='formInput' />
-                                <label htmlFor="show_solved" className='formLabel'>Show Solved (1/1 solved)</label>
+            <div className="Row">
+                <div className="Col-1">
+                    <div className="ChallFrame">
+                        <div className="frameBody">
+                            <div className="frameTitle">Filter</div>
+                            <div className="showSolved">
+                                <div className="formCheck">
+                                    <input type="checkbox" id="show_solved" className='formInput' checked={showSolved} onChange={handleShowSolvedChange} />
+                                    <label htmlFor="show_solved" className='formLabel'>Show Solved ({solvedCount}/{challenges.length} solved)</label>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="ChallFrame">
-                    <div className="frameBody">
-                        <div className="frameTitle">Categories</div>
-                        {
-                            Array.from(categoryCounts.entries()).sort((a, b) => a[0].localeCompare(b[0])).map(([category, { solved, total }]) => {
-                                return (
-                                    <div key={category} className='categoryCheck'>
-                                        <input id={`category-${category}`} data-category={category} className='formInput' type='checkbox' checked={categories[category]} onChange={handleCategoryCheckedChange} />
-                                        <label htmlFor={`category-${category}`} className='formLabel'>{category} ({solved}/{total} solved)</label>
-                                    </div>
-                                )
-                            })
-                        }
+                    <div className="ChallFrame">
+                        <div className="frameBody">
+                            <div className="frameTitle">Categories</div>
+                            <CategoryCheck category={CreateChallengeRequestDtoCategoryEnum.Forensic} />
+                            <CategoryCheck category={CreateChallengeRequestDtoCategoryEnum.Misc} />
+                            <CategoryCheck category={CreateChallengeRequestDtoCategoryEnum.Pwnable} />
+                            <CategoryCheck category={CreateChallengeRequestDtoCategoryEnum.Reversing} />
+                            <CategoryCheck category={CreateChallengeRequestDtoCategoryEnum.Web} />
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="ChallFrame">
-                {
-                    challengesToDisplay.map(challenge => {
-                        return (
-                            <Challenge
-                                key={Math.random()}
-                                challenge={challenge}
-                                solved={solvedChallengeTitles.includes(challenge.title!)}
-                                setSolved={setSolved}
-                            />
-                        )
-                    })
-                }
+                <div className="Col-2">
+                    {
+                        challengesToDisplay.map(challenge => {
+                            return (
+                                <Challenge
+                                    key={Math.random()}
+                                    challenge={challenge}
+                                    solved={challenge.solved!}
+                                    setSolved={setSolved}
+                                />
+                            )
+                        })
+                    }
+                </div>
             </div>
         </div>
     );
