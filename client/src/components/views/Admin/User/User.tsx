deee028f43224ea62ac416ab0cf0f14cac2cf7ca
Hu6r1s <darmaa333@gmail.com>
2023-05-14 19:01:32 +0900

    fix

--- client/src/components/views/Admin/User/User.tsx
+++ client/src/components/views/Admin/User/User.tsx
@@ -65,7 +65,7 @@ export default function User() {
           const key = `user-row-${username}`;
 
           return (
-            <tr key={key}>
+            <tr key={key} className='table-dark'>
               <td title={username}>{username}</td>
               <td title={nickname}>{nickname}</td>
               <td>
@@ -110,7 +110,7 @@ export default function User() {
 
   return (
     <>
-      <MDBCardHeader>
+      <MDBCardHeader className='text-white bg-dark'>
         <MDBPagination className="mb-0">
           <MDBPaginationItem>
             <MDBPaginationLink
@@ -125,24 +125,25 @@ export default function User() {
               label="Page"
               value={page}
               onChange={handleNumberInput(setPage)}
+              style={{ color: '#fff' }}
             />
           </MDBPaginationItem>
           <MDBPaginationItem>
-            <MDBPaginationLink>/ {totalPages}</MDBPaginationLink>
+            <MDBPaginationLink style={{ color: '#fff' }}>/ {totalPages}</MDBPaginationLink>
           </MDBPaginationItem>
           <MDBPaginationItem>
             <MDBPaginationLink
               href="#"
               onClick={() => checkedMovePage(page + 1)}
             >
-              <span aria-hidden="true">»</span>
+              <span aria-hidden="true" style={{ color: '#fff' }}>»</span>
             </MDBPaginationLink>
           </MDBPaginationItem>
         </MDBPagination>
       </MDBCardHeader>
-      <MDBTable color="dark" small>
+      <MDBTable small>
         <MDBTableHead>
-          <tr>
+          <tr className='table-dark'>
             <th scope="col">
               <MDBInput
                 contrast
